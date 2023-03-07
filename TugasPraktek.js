1c5fc2db0a9e33cb5f9707b3359ccb854c074cd8
Aryosatyo Bhanurahman <bhanurahman919@gmail.com>
2023-03-07 20:38:16 +0700

    Task Array

---
+++ TugasPraktek.js
@@ -0,0 +1,9 @@
+let barang =['ayam', 'botol', 'hape','kunci','dompet']
+let marvel =['spiderman', 'thor', 'magerman','hulk','loki']
+let kebutuhan =['makan', 'minum', 'tidur','tidur lagi','tidur terus']
+console.log(barang[1]);
+console.log(barang[3]);
+console.log(marvel[1]);
+console.log(marvel[3]);
+console.log(kebutuhan[1]);
+console.log(kebutuhan[3]);
---
+++ praktekArray.js
@@ -0,0 +1,6 @@
+let wadah = ['air','beras','gula','garam']
+console.log(wadah[0]);
+console.log(wadah[1]);
+console.log(wadah[2]);
+console.log(wadah[3]);
+console.log(wadah[wadah.length -2])
